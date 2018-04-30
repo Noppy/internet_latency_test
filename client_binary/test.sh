1be8b1bfcc8e8b66560f87c344f8b9070c6407a2
N.Fujita <Noppy@users.noreply.github.com>
2018-04-30 11:29:35 +0900

    Update test.sh

--- client_binary/test.sh
+++ client_binary/test.sh
@@ -7,14 +7,14 @@ MESSAGE="TEst"
 #thorough put test
 SIZE_MB=1
 
-title[0]=singapore;   host[0]=ec2-54-169-233-147.ap-southeast-1.compute.amazonaws.com
-title[1]=tokyo;       host[1]=ec2-13-230-60-207.ap-northeast-1.compute.amazonaws.com
-title[2]=osaka;       host[2]=ec2-3-208-140-176.ap-northeast-3.compute.amazonaws.com
-title[3]=oregon;      host[3]=ec2-34-214-208-17.us-west-2.compute.amazonaws.com
-title[4]=california;  host[4]=ec2-52-53-203-162.us-west-1.compute.amazonaws.com
-title[5]=virginia;    host[5]=ec2-54-152-142-225.compute-1.amazonaws.com
-title[6]=frankfurt;   host[6]=ec2-18-184-145-89.eu-central-1.compute.amazonaws.com
-title[7]=seoul;       host[7]=ec2-13-125-215-168.ap-northeast-2.compute.amazonaws.com
+title[0]=singapore;   host[0]=xx.xx.xx.xx.ap-southeast-1.compute.amazonaws.com
+title[1]=tokyo;       host[1]=xx.xx.xx.xx.ap-northeast-1.compute.amazonaws.com
+title[2]=osaka;       host[2]=xx.xx.xx.xx.ap-northeast-3.compute.amazonaws.com
+title[3]=oregon;      host[3]=xx.xx.xx.xx.us-west-2.compute.amazonaws.com
+title[4]=california;  host[4]=xx.xx.xx.xx.us-west-1.compute.amazonaws.com
+title[5]=virginia;    host[5]=xx.xx.xx.xx.compute-1.amazonaws.com
+title[6]=frankfurt;   host[6]=xx.xx.xx.xx.eu-central-1.compute.amazonaws.com
+title[7]=seoul;       host[7]=xx.xx.xx.xx.ap-northeast-2.compute.amazonaws.com
 LAST_NUM=7
 
 if [ "$(uname)" == 'Darwin' ];then
